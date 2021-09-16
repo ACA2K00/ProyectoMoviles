8e1fea17aeb49f3246e1edf3e118109a486f40d9
Alejandro Castro Arévalo <A01636235@itesm.mx>
2021-09-16 17:35:51 -0500

    Created RecipeVisualization and RecipeCreation (No functionality)

---
+++ app/src/main/java/com/emilianosloth/proyectofinal/CreateRecipeActivity.kt
@@ -0,0 +1,11 @@
+package com.emilianosloth.proyectofinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_recipe)
+    }
+}

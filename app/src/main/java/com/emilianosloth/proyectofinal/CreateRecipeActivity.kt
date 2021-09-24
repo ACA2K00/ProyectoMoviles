d5880f6b111e457d9821611070415f39e0c151e9
Alejandro Castro Arévalo <A01636235@itesm.mx>
2021-09-24 15:48:02 -0500

    Validated blank recipes

--- app/src/main/java/com/emilianosloth/proyectofinal/CreateRecipeActivity.kt
+++ app/src/main/java/com/emilianosloth/proyectofinal/CreateRecipeActivity.kt
@@ -54,25 +54,30 @@ class CreateRecipeActivity : AppCompatActivity() {
         }
 
         rUpBT.setOnClickListener{
-            val receta = hashMapOf(
-                "Autor" to Firebase.auth.currentUser?.email,
-                "Recipe Name" to recipeName.text.toString(),
-                "Ingredients" to enlist(recipeIngredients.text.toString()),
-                "Image" to recipeImage.text.toString(),
-                "Instructions" to recipeInstructions.text.toString(),
-                "Category" to spinner.getTag().toString()
-            )
+            if(recipeName.text.toString() == "" || recipeIngredients.text.toString() == "" ||
+                recipeImage.text.toString() == "" || recipeInstructions.text.toString() == ""){
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            }else{
+                val receta = hashMapOf(
+                    "Autor" to Firebase.auth.currentUser?.email,
+                    "Recipe Name" to recipeName.text.toString(),
+                    "Ingredients" to enlist(recipeIngredients.text.toString()),
+                    "Image" to recipeImage.text.toString(),
+                    "Instructions" to recipeInstructions.text.toString(),
+                    "Category" to spinner.getTag().toString()
+                )
 
-            db.collection("recetas").add(receta)
-                .addOnSuccessListener {
-                    Log.d("FIREBASE", "ID: ${it.id}")
-                    Toast.makeText(this, "Recipe Uploaded Correctly", Toast.LENGTH_SHORT).show()
-                }
-                .addOnFailureListener {
-                    Log.d("FIREBASE", "EXCEPTION: ${it.message}")
-                    Toast.makeText(this, "ERROR: COULDN'T UPLOAD RECIPE", Toast.LENGTH_SHORT).show()
-                }
-            finish()
+                db.collection("recetas").add(receta)
+                    .addOnSuccessListener {
+                        Log.d("FIREBASE", "ID: ${it.id}")
+                        Toast.makeText(this, "Recipe Uploaded Correctly", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Log.d("FIREBASE", "EXCEPTION: ${it.message}")
+                        Toast.makeText(this, "ERROR: COULDN'T UPLOAD RECIPE", Toast.LENGTH_SHORT).show()
+                    }
+                finish()
+            }
         }
     }
 
